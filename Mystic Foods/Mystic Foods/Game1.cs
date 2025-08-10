bfcb7ab3e19f6c0f360612720a26b957f339f8ee
BlueBun <min51aaa@gmail.com>
2025-08-10 11:47:21 +0700

    Added Main Menu System
    Same Main Menu as the previous work just added into the git project

--- Mystic Foods/Mystic Foods/Game1.cs
+++ Mystic Foods/Mystic Foods/Game1.cs
@@ -9,19 +9,29 @@ namespace Mystic_Foods
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        //asdsadadasd
-        // Test1 by wef
+        //Scene Management
+        private IGameScene _currentScene;
+        private MainMenuScene _mainMenuScene;
+        private GamePlayScene _gamePlayScene;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            //screen resolution
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 480;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            //make it start at main menu
+            _mainMenuScene = new MainMenuScene();
+            _gamePlayScene = new GamePlayScene();
+            _currentScene = _mainMenuScene;
 
             base.Initialize();
         }
@@ -31,12 +41,37 @@ namespace Mystic_Foods
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            //load scene
+            _mainMenuScene.LoadContent(Content);
+            _gamePlayScene.LoadContent(Content);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+
+            //Scene Logic
+            if (_currentScene == _mainMenuScene)
+            {
+                _mainMenuScene.Update(gameTime);
+                if (_mainMenuScene.StartGameRequested)
+                {
+                    _mainMenuScene.StartGameRequested = false;
+                    _currentScene = _gamePlayScene;
+                }
+                if (_mainMenuScene.ExitRequested)
+                {
+                    Exit();
+                }
+            }
+            else if (_currentScene == _gamePlayScene)
+            {
+                _gamePlayScene.Update(gameTime);
+                if (_gamePlayScene.BackToMenuRequested)
+                {
+                    _gamePlayScene.BackToMenuRequested = false;
+                    _currentScene = _mainMenuScene;
+                }
+            }
 
             // TODO: Add your update logic here
 
@@ -45,9 +80,10 @@ namespace Mystic_Foods
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            //draw scene
+            _currentScene.Draw(_spriteBatch);
 
             base.Draw(gameTime);
         }
---
+++ Mystic Foods/Mystic Foods/GamePlayScene.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystic_Foods
+{
+    public class GamePlayScene : IGameScene
+    {
+        private SpriteFont _font;
+        public bool BackToMenuRequested = false;
+        private KeyboardState _oldState;
+
+        public void LoadContent(ContentManager content)
+        {
+            _font = content.Load<SpriteFont>("MainFont");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Escape) && _oldState.IsKeyUp(Keys.Escape))
+                BackToMenuRequested = true;
+            _oldState = state;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.GraphicsDevice.Clear(Color.DarkSeaGreen);
+            spriteBatch.Begin();
+
+            string text = "Game Scene!\nPress ESC to return to the menu";
+            Vector2 size = _font.MeasureString(text);
+            spriteBatch.DrawString(
+                _font,
+                text,
+                new Vector2((800 - size.X) / 2, 200), //800- size.X /2 make it in the center of screen
+                Color.Black);
+
+            spriteBatch.End();
+        }
+    }
+}
