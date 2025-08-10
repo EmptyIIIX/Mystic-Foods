8358f31e2d2aab06926ad8e5d9722850bc96dc06
Empty[] <wefaj1234@gmail.com>
2025-08-10 15:50:38 +0700

    Drag & Drop Feature
    1. Create Drap&Drop.cs file
       - add class Drag_Drop
         - add method Contains() for check mouse's position on sprite
         - add method Draw() for draw sprite
       - add class Drag_DropManager
         - add method AddSprite() for add sprite
         - add method Update() for check drag and drop
       - add method Draw()
    
    2. Game1.cs
       - build object for Drag_DropManager
       - load constent at LoadContent()
       - import method Update() from class Drag_DropManager
       - Draw sprite from method Draw() in Drag_DropManager class

---
+++ Mystic Foods/Mystic Foods/Drag&Drop.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Mystic_Foods
+{
+    public class Drag_Drop
+    {
+        public string Name { get; }
+        public Texture2D Texture { get; }
+        public Vector2 Position { get; set; }
+        public Rectangle Bounds => new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height);
+        
+        public Drag_Drop(string name, Texture2D texture, Vector2 position)
+        {
+            Name = name;
+            Texture = texture;
+            Position = position;
+        }
+
+        //check mouse's position on sprite
+        public bool Contains(Point mousePosition)
+        {
+            return Bounds.Contains(mousePosition);
+        }
+        //Draw
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+
+    public class Drag_DropManager
+    {
+        private List<Drag_Drop> _sprites;
+        private Drag_Drop drag_Drop;
+        private MouseState _previousMouseState;
+
+        public Drag_DropManager()
+        {
+            _sprites = new List<Drag_Drop>();
+            _previousMouseState = Mouse.GetState();
+        }
+
+        //add sprite
+        public void AddSprite(Drag_Drop sprite)
+        {
+            _sprites.Add(sprite);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            MouseState ms = Mouse.GetState();
+            
+            if(ms.LeftButton == ButtonState.Pressed && _previousMouseState.LeftButton == ButtonState.Released) //check Drag
+            {
+                for (int i = _sprites.Count - 1; i >= 0; i--)
+                {
+                    var sprite = _sprites[i];
+                    if (sprite.Contains(ms.Position))
+                    {
+                        drag_Drop = sprite;
+                        Mouse.SetCursor(MouseCursor.Hand);
+                        break;
+                    }
+                }
+            }
+
+            if(drag_Drop != null && ms.LeftButton == ButtonState.Released) //check Drop
+            {
+                drag_Drop = null;
+                Mouse.SetCursor(MouseCursor.Arrow);
+            }
+
+            //update position sprite (Drag state)
+            if(drag_Drop != null)
+            {
+                drag_Drop.Position = new Vector2(ms.X - drag_Drop.Texture.Width / 2,
+                                                 ms.Y - drag_Drop.Texture.Height / 2);
+                Mouse.SetCursor(MouseCursor.Hand);
+            }
+            else
+            {
+                Mouse.SetCursor(MouseCursor.Arrow);
+            }
+
+            _previousMouseState = ms;
+        }
+
+        //draw all sprite
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach(var sprite in _sprites)
+            {
+                sprite.Draw(spriteBatch);
+            }
+        }
+    }
+}
--- Mystic Foods/Mystic Foods/Game1.cs
+++ Mystic Foods/Mystic Foods/Game1.cs
@@ -8,10 +8,7 @@ namespace Mystic_Foods
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
-        //asdsadadasd
-        // Test1 by wef
-
+        private Drag_DropManager _drag_dropManager;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -22,6 +19,11 @@ namespace Mystic_Foods
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.ApplyChanges();
+
+            _drag_dropManager = new Drag_DropManager();
 
             base.Initialize();
         }
@@ -29,8 +31,12 @@ namespace Mystic_Foods
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
             // TODO: use this.Content to load your game content here
+            //load texture , add sprite into Drag_DropManager
+            Texture2D boxTexture = Content.Load<Texture2D>("Assets");
+
+            //Lastest sprite will be top layer and first choosed for drag & drop
+            _drag_dropManager.AddSprite(new Drag_Drop("Assets", boxTexture, new Vector2(150, 150)));
         }
 
         protected override void Update(GameTime gameTime)
@@ -39,6 +45,7 @@ namespace Mystic_Foods
                 Exit();
 
             // TODO: Add your update logic here
+            _drag_dropManager.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -48,6 +55,9 @@ namespace Mystic_Foods
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _drag_dropManager.Draw(_spriteBatch);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
